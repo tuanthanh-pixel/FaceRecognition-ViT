e941da5bce4211bca442e1c762ca0f834d2abb33
Luong Tuan Thanh <luongtuanthanh2005@gmail.com>
2026-08-15 21:06:25 +0700

    test

--- src/test_pins.py
+++ src/test_pins.py
@@ -137,8 +137,8 @@ def build_loader(cfg):
 
 checkpoint_path = (
     "/kaggle/input/datasets/tritechsic/"
-    "checkpoint8/"
-    "sic_facevit_infonce_v2_best.pth"
+    "checkpoint9/"
+    "infonce_batch16_best.pth"
 )
 
 
